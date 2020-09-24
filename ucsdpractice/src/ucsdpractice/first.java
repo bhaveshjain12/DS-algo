49a5abe77035a1371da9dcd2de2d53563d5e5a5c
bhaveshjain12 <jainbhavesh617@gmail.com>
2020-09-24 23:33:02 +0530

    course

---
+++ ucsdpractice/src/ucsdpractice/first.java
@@ -0,0 +1,9 @@
+package ucsdpractice;
+
+public class first {
+public static void main(String[] args) {
+	double d=-77.0;
+	simplelocation lima=new simplelocation(-12.0,d);
+	simplelocation washDC=new simplelocation(-12.0,lima.lon);
+}
+}
---
+++ ucsdpractice/src/ucsdpractice/simplelocation.java
@@ -0,0 +1,13 @@
+package ucsdpractice;
+
+public class simplelocation{
+    double lat;
+	double lon;
+	
+	public simplelocation(double latIn, double lonIn) {
+		// TODO Auto-generated constructor stub
+		this.lat=latIn;
+		this.lon=lonIn;
+	}
+
+}
